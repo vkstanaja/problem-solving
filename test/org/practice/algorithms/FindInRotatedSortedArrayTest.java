72d782ac0f9b75411c8b81b36d6a9f07a4764fdd
vkstanaja <97565009+vkstanaja@users.noreply.github.com>
2022-01-13 00:14:14 -0800

    Added code for leetcode 33 - search in rotated array without duplicates

--- test/org/practice/algorithms/FindInRotatedSortedArrayTest.java
+++ test/org/practice/algorithms/FindInRotatedSortedArrayTest.java
@@ -1,17 +1,64 @@
 package org.practice.algorithms;
-import static org.junit.jupiter.api.Assertions.*;
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
 
-class FindInRotatedSortedArrayTest {
+@RunWith(JUnit4.class)
+public class FindInRotatedSortedArrayTest {
 
-    @org.junit.jupiter.api.BeforeEach
-    void setUp() {
+    private FindInRotatedSortedArray findInRotatedSortedArray;
+    int []nums;
+
+    public FindInRotatedSortedArrayTest() {
+
     }
 
-    @org.junit.jupiter.api.AfterEach
-    void tearDown() {
+    @Before
+    public void setUp() {
+        findInRotatedSortedArray = new FindInRotatedSortedArray();
     }
 
-    @org.junit.jupiter.api.Test
-    void search() {
+    @After
+    public void tearDown() {
+
+    }
+
+    @Test
+    public void searchInRightValidIndexRotatedArray() {
+        nums = new int[]{6, 7, 8, 9, 0, 1, 2, 3, 4, 5};
+        int targetIndex = findInRotatedSortedArray.search(nums, 3);
+        Assert.assertEquals(7, targetIndex);
+    }
+
+
+    @Test
+    public void searchInLeftValidIndexRotatedArray() {
+        nums = new int[]{6, 7, 8, 9, 0, 1, 2, 3, 4, 5};
+        int targetIndex = findInRotatedSortedArray.search(nums, 8);
+        Assert.assertEquals(2, targetIndex);
+    }
+
+    @Test
+    public void searchElementNotPresentInRotatedArray() {
+        nums = new int[]{6, 7, 8, 9, 0, 1, 2, 3, 4, 5};
+        int targetIndex = findInRotatedSortedArray.search(nums, -1);
+        Assert.assertEquals(-1, targetIndex);
+    }
+
+    @Test
+    public void searchValidOnSingleElementArray() {
+        nums = new int[]{6};
+        int targetIndex = findInRotatedSortedArray.search(nums, 6);
+        Assert.assertEquals(0, targetIndex);
+    }
+
+    @Test
+    public void searchOnEmptyArray() {
+        nums = new int[]{};
+        int targetIndex = findInRotatedSortedArray.search(nums, 1);
+        Assert.assertEquals(-1, targetIndex);
     }
 }
